562f5bf04bacfe213e56c4c1dccedb1a3c0f1f71
izzyandrade <israelcarneiro97@gmail.com>
2022-08-30 23:00:16 -0300

    Sending ethereum raw transactions to ganache

--- ethereum-fundamentals/sending-ethereum-transactions.js
+++ ethereum-fundamentals/sending-ethereum-transactions.js
@@ -2,20 +2,35 @@ import Web3 from 'web3';
 import EthereumJS from '@ethereumjs/tx';
 const web3 = new Web3('HTTP://127.0.0.1:7545');
 
-const sendingAddress = '0xE826a8accc7747790db787D5060CC5F0B6022AF8';
-const receivingAddress = '0x2e943B9022787f249A7a918A9Ce36F7c2304Ffcd';
+const sendingAddress = '0xE524046da6351cA495Dbfabe84a74Ed88795cF64';
+const receivingAddress = '0xF64d838f78c6d7671194f58eB1DE7AeF0B25E6c2';
+const privateKeySender =
+  '606438de17a8d7250c9bc34cb7324c4cf8f9af83dcee3adc52d446412f6488b3';
 
-const rawTransaction = {
-  nonce: 7,
-  to: receivingAddress,
-  gasPrice: 20000000,
-  gasLimit: 30000,
-  value: '0x' + Web3.utils.toBN(Web3.utils.toWei('1', 'ether')).toString(16),
-  data: '',
+const createRawTransaction = async (
+  sendingAddress,
+  receivingAddress,
+  etherValue,
+  data = ''
+) => {
+  const transactionCount = await web3.eth.getTransactionCount(sendingAddress);
+  return {
+    nonce: transactionCount,
+    to: receivingAddress,
+    gasPrice: 20000000,
+    gasLimit: 30000,
+    value:
+      '0x' +
+      web3.utils.toBN(Web3.utils.toWei(etherValue, 'ether')).toString(16),
+    data,
+  };
 };
 
-const privateKeySender =
-  'fdd7f5afbc702fa205446546a59afa1e25479c0931eba51e4032823ac35ddde8';
+const rawTransaction = await createRawTransaction(
+  sendingAddress,
+  receivingAddress,
+  '1'
+);
 const transaction = EthereumJS.Transaction.fromTxData(rawTransaction);
 const signedTx = transaction.sign(Buffer.from(privateKeySender, 'hex'));
 
